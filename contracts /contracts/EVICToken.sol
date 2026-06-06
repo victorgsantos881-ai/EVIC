c434253a068f58852b3f450f6a62f689e2759033
victorgsantos881-ai <victorgsantos881@gmail.com>
2026-06-06 17:45:15 +0100

    Create EVICToken.sol

---
+++ contracts /contracts/EVICToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EVICToken is ERC20, Pausable, Ownable {
+
+    constructor() ERC20("EVIC Token", "EVIC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
+
